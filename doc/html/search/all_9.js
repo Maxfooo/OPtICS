87f845ca0ed92036a385b6d7bc128a6ee65c32af
Max Ruiz <maxr@ophir.local>
2019-10-01 17:58:44 -0600

    updated documentation

--- doc/html/search/all_9.js
+++ doc/html/search/all_9.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['scavenge_24',['scavenge',['../classFileScavenger_1_1FileScavenger.html#ae3c1cb23f5d242690c37c0304c9b14db',1,'FileScavenger::FileScavenger']]],
-  ['sortobjecthierarchy_25',['sortObjectHierarchy',['../classObjectFormatter_1_1ObjectFormatter.html#a36869ac7d8aca03f468ba1139984099c',1,'ObjectFormatter::ObjectFormatter']]],
+  ['sortobjecthierarchy_25',['sortObjectHierarchy',['../classObjectFormatter_1_1ObjectFormatter.html#ae165f66545a0104eed18167f3ef66663',1,'ObjectFormatter::ObjectFormatter']]],
   ['startstructobj_26',['startStructObj',['../classFileParser_1_1FileParser.html#acad60015ffe026ad25104c31046ea7ac',1,'FileParser::FileParser']]],
   ['structtodtype_27',['structToDtype',['../classObjectFormatter_1_1ObjectFormatter.html#ac10c0f5fbe53ff0a870f1478ede32107',1,'ObjectFormatter::ObjectFormatter']]]
 ];
